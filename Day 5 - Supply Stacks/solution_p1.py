e91055ffc17aff03017aaa5d6a1d4153d96f4b5b
Tommy <tommaso.vuaran@gmail.com>
2022-12-06 20:58:37 +0100

    Day 5: Supply Stacks

---
+++ Day 5 - Supply Stacks/solution_p1.py
@@ -0,0 +1,22 @@
+"""
+Advent of Code 2022
+Day 5: Supply Stacks (Part One)
+"""
+
+stacks = {}
+with open("stacks.txt", encoding='UTF-8') as input_file:
+    for line in input_file:
+        stack_index, crates = line.strip().split()
+        stacks[stack_index] = list(crates)
+
+with open("moves.txt", encoding='UTF-8') as input_file:
+    for line in input_file:
+        splitted_line = line.strip().split()
+        crates = int(splitted_line[1])
+        from_stack = splitted_line[3]
+        to_stack = splitted_line[5]
+        for _ in range(crates):
+            stacks[to_stack].append(stacks[from_stack].pop())
+
+top_crates = [stacks[str(i+1)][-1] for i in range(len(stacks.keys()))]
+print(f"Top crates: {''.join(top_crates)}")
