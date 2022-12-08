24f2535124ac7e62c96790037b9c761179db4864
Tommy <tommaso.vuaran@gmail.com>
2022-12-08 23:51:13 +0100

    Day 7: No Space Left On Device

---
+++ Day 7 - No Space Left On Device/solution_p1.py
@@ -0,0 +1,56 @@
+"""
+Advent of Code 2022
+Day 7: No Space Left On Device (Part One)
+"""
+
+def set_directory(path, dir_name):
+    current_file_tree = file_tree
+    for p in path:
+        current_file_tree = current_file_tree[p]
+    current_file_tree[dir_name] = {}
+
+def set_file(path, file_name, file_size):
+    current_file_tree = file_tree
+    for p in path:
+        current_file_tree = current_file_tree[p]
+    current_file_tree[file_name] = file_size
+
+def sum_sizes(tree):
+    tree_size = 0
+    tree_sum = 0
+    for subtree in tree:
+        if not type(tree[subtree]) == dict:
+            tree_size += int(tree[subtree])
+        else:
+            subtrees_sum, subtree_size = sum_sizes(tree[subtree])
+            tree_size += subtree_size
+            tree_sum += subtrees_sum
+    if tree_size <= max_size:
+        tree_sum += tree_size
+    return tree_sum, tree_size
+
+max_size = 100000
+current_path = []
+file_tree = {'/': {}}
+
+with open("input.txt", encoding='UTF-8') as input_file:
+    for line in input_file:
+        splitted_line = line.strip().split()
+        if splitted_line[0] == '$':
+            if splitted_line[1] == 'cd':
+                if splitted_line[2] == '/':
+                    current_path = ['/']
+                elif splitted_line[2] == '..':
+                    current_path.pop()
+                else:
+                    current_path.append(splitted_line[2])
+        else:
+            if splitted_line[0] == 'dir':
+                set_directory(current_path, splitted_line[1])
+            else:
+                set_file(current_path, splitted_line[1], splitted_line[0])
+
+total_sum, _ = sum_sizes(file_tree)
+
+
+print(f"Sum of total sizes: {total_sum}")
